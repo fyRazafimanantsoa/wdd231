fb89473f5e8323383978f983c7c065f247ec08a5
fyRazafimanantsoa <fyratsoa@gmail.com>
2026-09-07 16:03:03 +0300

    working on the navbar and the general content of the webpage

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,14 @@
+const pageName = document.getElementById('pageName');
+const syncPageName = document.getElementById('syncPage');
+syncPageName.textContent = pageName.textContent;
+
+const navigation = document.querySelector('.nav');
+const mainContent = document.querySelector('main');
 let btnMenu = document.getElementById('ham-btn');
 
 btnMenu.addEventListener('click', () => {
     btnMenu.classList.toggle('show');
+    navigation.classList.toggle('drop');
+    mainContent.classList.toggle('pushdown');
 });
+
